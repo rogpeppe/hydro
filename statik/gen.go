cb3ef9867f3bb8ef879032e4a64207863f3cfe7c
Roger Peppe <rogpeppe@gmail.com>
2017-02-04 18:49:22 +0000

    add dependencies file
    Also use proposed version of statik tool with compress flag.

--- statik/gen.go
+++ statik/gen.go
@@ -3,4 +3,4 @@ package statik
 // TODO make the "statik" tool general enough that we can call this
 // package "static" not "static"
 
-//go:generate sh -c "statik -src data && mv statik/*.go . && rmdir statik"
+//go:generate sh -c "statik -compress -src data && mv statik/*.go . && rmdir statik"
