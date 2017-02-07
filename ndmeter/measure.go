60827f75719d33dbeae9f31eea09adb351d43019
Roger Peppe <rogpeppe@gmail.com>
2017-02-07 00:26:55 +0000

    ndmeter: implement polling
    We can now pull current data from a meter
    and obtain samples over time without
    worrying about meters that might take forever to
    respond.

---
+++ ndmeter/measure.go
@@ -0,0 +1,133 @@
+package ndmeter
+
+import (
+	"bufio"
+	"math"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	errgo "gopkg.in/errgo.v1"
+)
+
+//go:generate stringer -type measure
+
+type measure int
+
+const (
+	_ measure = iota
+	mPhase1Current
+	mPhase2Current
+	mPhase3Current
+	mPhase1Voltage
+	mPhase2Voltage
+	mPhase3Voltage
+	mPhase1PowerFactor
+	mPhase2PowerFactor
+	mPhase3PowerFactor
+	mSystemkvar
+	mSystemkW
+	mSystemkWhHighWord
+	mSystemkWhLowWord
+	mSystemkvarhHighWord
+	mSystemkvarhLowWord
+	mPhase1kW
+	mPhase2kW
+	mPhase3kW
+	mCurrentScale
+	mVoltsScale
+	mPowerScale
+	mEnergyScale
+	mSystemkWh
+	mSystemkvarh
+)
+
+var measureLinePat = regexp.MustCompile(`<td id='([^']+)'>([^<]*)</td>`)
+
+func Get(host string) (Reading, error) {
+	resp, err := http.Get("http://" + host + "/Values_live.shtml")
+	if err != nil {
+		return Reading{}, errgo.Notef(err, "cannot fetch live values")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return Reading{}, errgo.Newf("error status fetching live values: %v", resp.Status)
+	}
+	scan := bufio.NewScanner(resp.Body)
+	measures := make(map[measure]int)
+	for scan.Scan() {
+		parts := measureLinePat.FindStringSubmatch(scan.Text())
+		if len(parts) != 3 {
+			continue
+		}
+		m, ok := measureNames[parts[1]]
+		if !ok {
+			continue
+		}
+		val, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return Reading{}, errgo.Newf("unexpected measure value in %q", scan.Text())
+		}
+		measures[m] = val
+	}
+	systemkW, err := getVal(measures, mSystemkW, mPowerScale)
+	if err != nil {
+		return Reading{}, errgo.Newf("cannot read system power")
+	}
+	activeEnergy, err := getVal(measures, mSystemkWh, mEnergyScale)
+	if err != nil {
+		return Reading{}, errgo.Newf("cannot read total energy")
+	}
+	return Reading{
+		ActivePower: systemkW * 1000,
+		TotalEnergy: activeEnergy * 1000,
+	}, nil
+}
+
+type Reading struct {
+	// ActivePower holds the currently generated
+	// power in W.
+	ActivePower float64
+	// TotalEnergy holds the total generated energy
+	// in WH.
+	TotalEnergy float64
+}
+
+func getVal(m map[measure]int, key, scale measure) (float64, error) {
+	v, ok := m[key]
+	if !ok {
+		return 0, errgo.Newf("no key found")
+	}
+	sv, ok := m[scale]
+	if !ok {
+		return 0, errgo.Newf("no scale found")
+	}
+	return float64(v) * math.Pow(10, float64(sv)-6), nil
+}
+
+var measureNames = map[string]measure{
+	"ap":     mSystemkW,
+	"rp":     mSystemkvar,
+	"v1":     mPhase1Voltage,
+	"i1":     mPhase1Current,
+	"pf1":    mPhase1PowerFactor,
+	"p1":     mPhase1kW,
+	"v2":     mPhase2Voltage,
+	"i2":     mPhase2Current,
+	"pf2":    mPhase2PowerFactor,
+	"p2":     mPhase2kW,
+	"v3":     mPhase3Voltage,
+	"i3":     mPhase3Current,
+	"pf3":    mPhase3PowerFactor,
+	"p3":     mPhase3kW,
+	"ae":     mSystemkWh,
+	"re":     mSystemkvarh,
+	"ascale": mCurrentScale,
+	"vscale": mVoltsScale,
+	"pscale": mPowerScale,
+	"escale": mEnergyScale,
+	// Unknown names:
+	//	ct=150
+	//	nv=480
+	//	ps=1
+}
---
+++ ndmeter/sampler.go
@@ -0,0 +1,97 @@
+package ndmeter
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"go4.org/syncutil/singleflight"
+)
+
+func NewSampler() *Sampler {
+	return &Sampler{
+		recent: make(map[string]*Sample),
+	}
+}
+
+// Sampler allows the sampling of a set of meters over time.
+type Sampler struct {
+	group  singleflight.Group
+	mu     sync.Mutex
+	recent map[string]*Sample
+}
+
+// Sample holds a meter reading that was received at
+// a particular time.
+type Sample struct {
+	Time time.Time
+	Reading
+}
+
+type result struct {
+	index  int
+	sample *Sample
+}
+
+// GetAll tries to acquire a sample for the meters at all the given
+// addresses. If the context is cancelled, it will return immediately
+// with the most recent data that it has acquired, which might be from
+// an earlier time. The returned slice will hold the result for each
+// respective address in addrs. Nil elements will be returned when no
+// data has ever been acquired for an address.
+func (sampler *Sampler) GetAll(ctx context.Context, addrs ...string) []*Sample {
+	results := make(chan result, len(addrs))
+	for i, addr := range addrs {
+		i, addr := i, addr
+		go func() {
+			s := sampler.getOne(ctx, addr)
+			sampler.mu.Lock()
+			defer sampler.mu.Unlock()
+			sampler.recent[addr] = s
+			results <- result{
+				index:  i,
+				sample: s,
+			}
+		}()
+	}
+	samples := make([]*Sample, len(addrs))
+	numSamples := 0
+	for numSamples < len(samples) {
+		select {
+		case <-ctx.Done():
+			// Fill any samples with previously retrieved data when we have some.
+			sampler.mu.Lock()
+			defer sampler.mu.Unlock()
+			for i, s := range samples {
+				if s == nil {
+					samples[i] = sampler.recent[addrs[i]]
+				}
+			}
+			return samples
+		case s := <-results:
+			samples[s.index] = s.sample
+			numSamples++
+		}
+	}
+	return samples
+}
+
+func (sampler *Sampler) getOne(ctx context.Context, addr string) *Sample {
+	for ctx.Err() == nil {
+		sample0, err := sampler.group.Do(addr, func() (interface{}, error) {
+			reading, err := Get(addr)
+			return &Sample{
+				Time:    time.Now(),
+				Reading: reading,
+			}, err
+		})
+		sample := sample0.(*Sample)
+		if err == nil {
+			return sample
+		}
+		log.Printf("failed to get reading from %s: %v", addr, err)
+	}
+	log.Printf("failed to get reading from %s: %v", addr, ctx.Err())
+	return nil
+}
